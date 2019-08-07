e8a89d364001557fe3d820be490c4fb68ed3ab29
Kfir <kfir1g@cs.huji.ac.il>
2019-08-07 17:42:37 +0300

    Multi-threading for tree creation

--- create_train_trees.py
+++ create_train_trees.py
@@ -1,21 +1,46 @@
 from tree import *
 import pandas as pd
 import math
+from multiprocessing import Pool
 
 
 # INPUT_PATH = "data/results_2018.csv"
 INPUT_PATH = "dataa.csv"
+GOAL = "Goal"
 TRAIN_LEVEL = 0.5
+TREE = "tree"
+ENTROPY = "entropy"
+INFORMATION_GAIN = "information_gain"
+INFORMATION_RATIO = "information_ratio"
+
+
+def tree_creation(type, records_df, limit=0, attributes=None, goal=GOAL):
+    """"""
+    if type == TREE:
+        return Tree(records_df, limit, attributes, goal)
+    elif type == ENTROPY:
+        return EntropyTree(records_df, limit, attributes, goal)
+    elif type == INFORMATION_GAIN:
+        return InformationGainTree(records_df, limit, attributes, goal)
+    elif type == INFORMATION_RATIO:
+        return InformationRatioTree(records_df, limit, attributes, goal)
+    else:
+        return
 
 
 def create_trees(training_data, goal="pickups"):
     """"""
-    all_trees = []
-    all_trees.append(Tree(training_data, goal=goal))
-    all_trees.append(EntropyTree(training_data, goal=goal))
-    all_trees.append(InformationGainTree(training_data, goal=goal))
-    all_trees.append(InformationRatioTree(training_data, goal=goal))
-    return all_trees
+    p = Pool(2)
+    res = p.starmap(tree_creation, [(TREE, training_data, 0, None, goal),
+                                    (INFORMATION_GAIN, training_data, 0, None, goal),
+                                    (ENTROPY, training_data, 0, None,
+                                     goal)])
+    p.close()
+    p.join()
+
+    for t in res:
+        generate_graph(t)
+    return res
 
 
 def create_file(test_data, all_trees, goal):
@@ -52,11 +77,11 @@ if __name__ == "__main__":
     test_data = data[~data.isin(training_data)].dropna()
 
     # create trees based on training data
-    all_trees = create_trees(training_data, "Goal")
+    all_trees = create_trees(training_data, GOAL)
 
     # create file
-    create_file(test_data, all_trees, "Goal")
+    create_file(test_data, all_trees, GOAL)
 
     # Save data
-    # export_trees(all_trees)
-    # export_training_and_test(training_data, test_data)
+    export_trees(all_trees)
+    export_training_and_test(training_data, test_data)
