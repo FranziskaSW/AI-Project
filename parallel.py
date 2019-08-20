edf3522ef4530c08ac6e79f2be78988d69356a7a
kfir1g <46302116+kfir1g@users.noreply.github.com>
2019-08-20 15:02:00 +0300

    Delete parallel.py

--- parallel.py
+++
@@ -1,7 +0,0 @@
-def parallel_function(function_to_apply, list_of_inputs, num_threads=None):
-    from multiprocessing import Pool
-    pool = Pool(processes=num_threads)
-    result = pool.map(function_to_apply, list_of_inputs)
-    pool.close()
-    pool.join()
-    return result
